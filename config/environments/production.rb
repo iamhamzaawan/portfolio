f1f4dadec6c8e9faf877a9f573d3bf7bb642efa7
iamhamzaawan <hamza_awan1996@hotmail.com>
2021-11-18 06:58:54 +0500

    Added favicon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "hamzaawan.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -31,13 +32,13 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)  
-config.serve_static_assets = false
+  config.serve_static_assets = false
 
-# Compress JavaScripts and CSS  
-config.assets.compress = true
+  # Compress JavaScripts and CSS  
+  config.assets.compress = true
 
-# Generate digests for assets URLs  
-config.assets.digest = true
+  # Generate digests for assets URLs  
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
